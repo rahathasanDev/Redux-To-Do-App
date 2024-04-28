ba466bcdb28e42d54e1d36f3b32e4a61eeb18878
rahathasanDev <devrahat.hasan@gmail.com>
2024-04-28 12:29:00 +0600

    Typescript and redux installation complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "./components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <h1>My todo app</h1>
+    <Button>Button</Button>
+    
     </>
   )
 }
