ec541481d14797e3b3e752b3a42d056c21580edd
rahathasanDev <devrahat.hasan@gmail.com>
2024-04-28 12:37:21 +0600

    store and todoslice connected

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import todoReducer from './features/todoSlice'
 
 export const store = configureStore({
   reducer: {
-   
+   todos:todoReducer,
   },
 })
 
