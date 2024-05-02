58ee75141457de0fec2ef4c8eeebca20ca6786e6
rahathasanDev <devrahat.hasan@gmail.com>
2024-05-02 23:51:56 +0600

    Todo card added

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -3,16 +3,17 @@ import TodoCard from "./TodoCard";
 const TodoContainer = () => {
   return (
     <div>
-     <div>
-      <button>Add todo</button>
-      <button>Filter</button>
-     </div>
-     <div className="bg-red-500 w-full rounded-xl p-5 h-[500px] space-y-5">
-      <TodoCard></TodoCard>
-      <TodoCard></TodoCard>
-      <TodoCard></TodoCard>
-
-     </div>
+      <div>
+        <button>Add todo</button>
+        <button>Filter</button>
+      </div>
+      <div className="bg-red-500 w-full rounded-xl p-5 h-[500px] space-y-5">
+        <TodoCard></TodoCard>
+        <TodoCard></TodoCard>
+        <TodoCard></TodoCard>
+        <TodoCard></TodoCard>
+        <TodoCard></TodoCard>
+      </div>
     </div>
   );
 };
